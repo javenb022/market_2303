b872d7b26e2c75c70221d3601f0b7fd7d8639693
Javen Wilson <javenb022@gmail.com>
2023-04-17 12:00:06 -0400

    Add initialize tests

---
+++ spec/market_spec.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+RSpec.describe Market do
+  before(:each) do
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@market).to be_a(Market)
+    end
+  end
+end
