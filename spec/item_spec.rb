7a91e3eb14708a6b3c3246f98b47a5623b98fb53
Javen Wilson <javenb022@gmail.com>
2023-04-17 11:21:23 -0400

    Adds initialize and reading attributes tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,23 @@
 require 'rspec'
+require './lib/item'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1  = Item.new({name: 'Peach', price: "$0.75"})
+    @item2  = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@item1).to be_a(Item)
+      expect(@item2).to be_a(Item)
+    end
+  end
+
+  describe '#reads attributes' do
+    it 'reads attributes' do
+      expect(@item2.name).to eq("Tomato")
+      expect(@item2.price).to eq(0.5)
+    end
+  end
+end
