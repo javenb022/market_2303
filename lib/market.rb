f2368fea17d25d451cfc4bb3bebb202d97f09055
Javen Wilson <javenb022@gmail.com>
2023-04-17 13:02:37 -0400

    Add sorted_item_list method

--- lib/market.rb
+++ lib/market.rb
@@ -13,4 +13,14 @@ class Market
   def vendor_names
     @vendors.map {|vendor| vendor.name}
   end
+
+  def sorted_item_list
+    sorted_list = []
+    @vendors.flat_map do |vendor|
+      vendor.inventory.map do |item|
+        sorted_list << item[0].name
+      end
+    end
+    sorted_list.uniq.sort
+  end
 end
