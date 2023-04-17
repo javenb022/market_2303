b7429f63461f3f1d2202665d2b25f3e9321bfb20
Javen Wilson <javenb022@gmail.com>
2023-04-17 12:14:54 -0400

    Add attributes, adds merket vendors tests

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Market do
   before(:each) do
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: "$0.50"})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
@@ -15,6 +17,40 @@ RSpec.describe Market do
   describe '#initialize' do
     it 'exists' do
       expect(@market).to be_a(Market)
+    end
+  end
+
+  describe '#attributes' do
+    it 'reads attributes of market' do
+      expect(@market.name).to eq("South Pearl Street Farmers Market")
+    end
+  end
+
+  before(:each) do
+    @vendor1.stock(@item1, 35)    
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)    
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)  
+  end
+  
+  describe '#adds market vendors' do
+    it 'adds market vendors' do
+      expect(@market.vendors).to eq([])
+
+      @market.add_vendor(@vendor1)    
+      @market.add_vendor(@vendor2)    
+      @market.add_vendor(@vendor3)
+      
+      expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    end
+
+    it 'reads market vendors names' do
+      @market.add_vendor(@vendor1)    
+      @market.add_vendor(@vendor2)    
+      @market.add_vendor(@vendor3)
+      
+      expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
   end
 end
