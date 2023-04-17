b95d4441b6e466de657bcf2c00c326f1dc8dddcc
Javen Wilson <javenb022@gmail.com>
2023-04-17 11:54:41 -0400

    Add check_stock and stock methods

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,5 +1,20 @@
 class Vendor
+  attr_reader :name,
+              :inventory
   def initialize(name)
     @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    if @inventory.include?(item)
+      @inventory[item]
+    else
+      0
+    end
+  end
+
+  def stock(product, amount)
+    @inventory[product] += amount
   end
 end
