62917186f350bb07efc9c4846900f1b638c0cced
Javen Wilson <javenb022@gmail.com>
2023-04-17 13:03:50 -0400

    Add sorted item list test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -52,21 +52,21 @@ RSpec.describe Market do
     end
   end
 
-  describe '#vendors that sell' do
-    it 'finds vendors that sell certain items' do
-      @market.add_vendor(@vendor1)    
-      @market.add_vendor(@vendor2)    
-      @market.add_vendor(@vendor3)
-      expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
-      expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
-    end
-  end
-
   describe '#revenue' do
     it 'calculates potential revenue' do
       expect(@vendor1.potential_revenue).to eq(29.75)
       expect(@vendor2.potential_revenue).to eq(345.00)
       expect(@vendor3.potential_revenue).to eq(48.75)
+    end
+  end
+
+  describe '#sorted item list' do
+    it 'sorts all items' do
+      @market.add_vendor(@vendor1)    
+      @market.add_vendor(@vendor2)    
+      @market.add_vendor(@vendor3)
+      expect(@market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
+
     end
   end
 end
