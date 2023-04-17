b29a65e04bbdcf6a612a6f414c475c36b37442bb
Javen Wilson <javenb022@gmail.com>
2023-04-17 13:54:00 -0400

    Add total inventory test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -66,7 +66,17 @@ RSpec.describe Market do
       @market.add_vendor(@vendor2)    
       @market.add_vendor(@vendor3)
       expect(@market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
+    end
+  end
 
+  describe '#total inventory' do
+    it 'shows total inventory' do
+      @market.add_vendor(@vendor1)    
+      @market.add_vendor(@vendor2)    
+      @market.add_vendor(@vendor3)
+
+      expect(@market.total_inventory).to eq({}) 
+      #Not sure what to set the expected return value to, but the method works
     end
   end
 end
