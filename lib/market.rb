fb54bfa8f477ae4be49b1ba3cb8de1cb3b0d08f1
Javen Wilson <javenb022@gmail.com>
2023-04-17 13:53:36 -0400

    Add total_inventory method

--- lib/market.rb
+++ lib/market.rb
@@ -16,11 +16,35 @@ class Market
 
   def sorted_item_list
     sorted_list = []
-    @vendors.flat_map do |vendor|
+    @vendors.map do |vendor|
       vendor.inventory.map do |item|
         sorted_list << item[0].name
       end
     end
     sorted_list.uniq.sort
+  end
+
+
+  def total_inventory
+    item_quantities = Hash.new(0)
+    @vendors.map do |vendor|
+      vendor.inventory.map do |item|
+        item_quantities[item[0]] = {:quantity => 0, :vendors => []}
+      end
+    end
+    item_quantity = 0
+    @vendors.map do |vendor|
+      vendor.inventory.map do |item|
+        item_quantity += vendor.check_stock(item[0])
+        item_quantities[item[0]][:quantity] += item_quantity
+      end
+    end
+    @vendors.map do |vendor|
+      vendor.inventory.map do |item|
+        item_quantities[item[0]][:vendors] << vendor
+      end
+    end
+    require 'pry'; binding.pry
+    item_quantities
   end
 end
